2cc493826608bfd7ac2c888a1e1ad26592d53ebd
Red Daly <red@condi.Stanford.EDU>
2009-06-25 09:49:23 -0700

    Parenscript utilities.

---
+++ src/package.lisp
@@ -0,0 +1,30 @@
+(defpackage org.iodb.paren-util
+  (:nicknames #:paren-util)
+  (:use :common-lisp :parenscript)
+  (:export
+   #:in-package
+   #:use-package
+   #:defaultf
+   #:funcall
+   #:methcall
+   #:with-arguments-array
+   #:length
+   #:subseq
+   #:to-array
+   #:merge-into
+   #:lispy-apply
+   #:lispy-map
+   #:collect
+   #:every
+   #:some
+   #:remove-duplicates
+   #:lexicographic-compare))
+
+(defpackage #:js-global
+    (:export #:console #:console.warn #:window #:window.console #:document #:arguments
+	     #:splice
+	     #:-Array #:-Object))
+
+
+(defpackage org.iodb.paren-util.js-package
+  (:use :parenscript :common-lisp))
